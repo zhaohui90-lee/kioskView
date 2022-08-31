89790bec2a2b374676c1b6b7f828c0538e5ba148
lizhaohui <fhenn@163.com>
2022-08-31 19:27:46 +0800

    项目初始化

---
+++ src/views/Test.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h1>message: {{ msg }}</h1>
+  </div>
+</template>
+
+
+<script setup lang="ts">
+import axios from 'axios'
+import { computed, ref, toRef } from 'vue';
+
+let msgRaw: String;
+axios.get('http://localhost:3000/test')
+  .then(res => {
+    msgRaw = res.data
+  })
+  .catch(error => {
+    console.log(error);
+
+  })
+
+
+
+
+</script>
+
+<style scoped>
+h1,
+h2 {
+  font-weight: normal;
+}
+</style>
