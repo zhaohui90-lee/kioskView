8d280936a79cc9d65c10453c85ac95f1a8e3f259
lizhaohui <fhenn@163.com>
2022-09-07 20:40:47 +0800

    读卡模块编写

--- src/views/card/CardModuleView.vue
+++ src/views/card/CardModuleView.vue
@@ -7,7 +7,11 @@ import ContentSlot from "@/slot/content/ContentSlot.vue";
   <ContentSlot>
     <template #main-content>
       <div class="card-module">
-        <div class="card-module-title">请选择卡类型</div>
+        <div class="card-module-title">
+          <div class="title-name">
+            <span>请选择卡类型</span>
+          </div>
+        </div>
         <div class="card-module-template">
           <div class="card-item" v-for="item in 4" :key="item">读卡模板</div>
         </div>
@@ -21,21 +25,36 @@ import ContentSlot from "@/slot/content/ContentSlot.vue";
   height: 100%;
   width: 100%;
   background: #fff;
+  .title-name {
+    padding: 15px 0;
+    span {
+      display: inline-block;
+      height: 80px;
+      width: 300px;
+      line-height: 80px;
+      font-size: 40px;
+      border-radius: 30px;
+      color: var(--main);
+      background: var(--main_opacity_2);
+      border-bottom: 10px solid var(--main_opacity_5);
+    }
+  }
 
   &-title {
-    height: 10%;
+    height: 15%;
     font-size: 35px;
     text-align: center;
     border-bottom: 1px solid #ccc;
   }
   &-template {
-    height: 90%;
+    height: 85%;
     display: flex;
     flex-wrap: wrap;
+    margin: 40px 125px 0 125px;
     
     .card-item {
-      width: 500px;
-      height: 300px;
+      width: 460px;
+      height: 224px;
     }
   }
 }
