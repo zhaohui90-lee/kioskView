0b1f81c6fd5cb9c863b4d2978ea10986687c8da7
lizhaohui <fhenn@163.com>
2022-10-20 14:21:25 +0800

    修改计时写法

--- src/components/ClockTimer.vue
+++ src/components/ClockTimer.vue
@@ -2,30 +2,38 @@
 import { ref, type Ref } from 'vue';
 import moment from 'moment'
 
-let hour: Ref = ref<String>(moment().format('HH'))
-let minute: Ref = ref<String>(moment().format('mm'))
+let now = moment().utc().utcOffset(8)
+let hour: Ref = ref<String>(now.format('HH'))
+let minute: Ref = ref<String>(now.format('mm'))
+let second: Ref = ref<String>(now.format('ss'))
 
 setInterval(() => {
   hour.value = moment().format('HH')
   minute.value = moment().format('mm')
-}, 60 * 1000)
+  second.value = moment().format('ss')
+}, 1000)
 </script>
   <template>
   <div class="timer-temp">
     <div class="timer-temp-hour">{{ hour }}</div>
     <div class="timer-temp-minute">:{{ minute }}</div>
+    <div class="timer-temp-second">:{{ second }}</div>
   </div>
 </template>
   <style lang="less" scoped>
   .timer-temp {
-    color: var(--pink);
+    color: var(--black);
   
     &-hour {
       display: inline-block;
-      font-size: 60px;
+      font-size: 36px;
     }
   
     &-minute {
+      display: inline-block;
+      font-size: 36px;
+    }
+    &-second {
       display: inline-block;
       font-size: 36px;
     }
