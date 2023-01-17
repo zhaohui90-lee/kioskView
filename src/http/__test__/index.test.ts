925505adacea7f163e1925137b2dff5bed4505cf
lizhaohui <fhenn@163.com>
2023-01-17 14:11:49 +0800

    新增单元测试

---
+++ src/http/__test__/index.test.ts
@@ -0,0 +1,10 @@
+import { describe, expect, test } from "@jest/globals";
+import { fetchPost } from "../index";
+
+describe('fetchPost module', () => {
+  test('fetch ...', () => {
+    return fetchPost('https://www.baidu.com', {}).then(data => {
+      expect(data).toBe('...')
+    })
+  })
+})
